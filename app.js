2e5dd8330162367c52bc0acf885b481a770f9382
Gilbert <akhamgilbert@gmail.com>
2023-08-06 21:11:58 +0530

    Airtable api to perform CRUD operation

---
+++ app.js
@@ -0,0 +1,59 @@
+var Airtable = require('airtable');
+const dotenv = require('dotenv')
+dotenv.config();
+Airtable.configure({
+    apiKey:process.env.AIRTABLE_API_KEY
+})
+const base = Airtable.base('appiNDoKSEV2K5yFf')
+
+// this is to get a record from table
+base('tblhZWtvvNSAegrdN').select({
+    maxRecords:2,
+}).eachPage(function page(records,fetchNextPage){
+    records.forEach(function(record){
+        console.log('Details' + ' ' + record.get('Name') + ' ' + record.get('Age'));
+    })
+    fetchNextPage();
+},function done(err){
+    if(err){
+        console.error(err); return;
+    }
+})
+
+//to create a record in a table
+base('tblhZWtvvNSAegrdN').create([
+    {
+        "fields":{
+            "Name":"Smith",
+            "Age":"16"
+        }
+    },
+    {
+        "fields":{
+            "Name":"Tom",
+            "Age":"32"
+        }
+    }
+]).then(record =>{
+    console.log('Create record:',record);
+}).catch(err => {
+    console.error('Error creating record:',err);
+})
+
+// update records in a table using id
+base('tblhZWtvvNSAegrdN').update([
+    {
+        "id" : "recPHU4xUDUsE9mlT",
+        "fields":{
+            "Name":"Harry",
+            "Age":"40"
+        }
+    }
+]).then(record => {
+    console.log("Record is updated:",record);
+}).catch(err => {
+    console.log("Error in updating:",err);
+})
+
+
+// this is to delete a record from the table
